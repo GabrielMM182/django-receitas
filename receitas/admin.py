c7d65964447033e46c4da7edf1d854e21360920e
GabrielMM182 <gmm_morais@hotmail.com>
2022-01-15 01:00:06 -0300

    imgs adicionadas

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'date_receita')
+    list_display = ('id', 'nome_receita', 'categoria', 'date_receita', 'publicada')
     list_display_links = ('id', 'nome_receita', 'categoria')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
     list_per_page = 5
 
 admin.site.register(Receita, ListandoReceitas)
--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,8 @@ from .models import Receita
 
 def index(request):
 
-    receitas = Receita.objects.all()
+    # receitas = Receita.objects.all() mostra todos sem filtro
+    receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
 
     dados = { # vai trazer a receita
         'receitas' : receitas
